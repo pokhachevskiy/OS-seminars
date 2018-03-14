b15bbbc0107d5dba15a836f250294b295ce4868a
Daniil <pohachevskiy.da@phystech.edu>
2018-03-14 21:25:08 +0300

    working pi

--- pi.c
+++ pi.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h> //-lm
-int numberOfthreads = 5;
+int numberOfthreads = 5; //default value
 
 
 
-double* result;//[numberOfthreads];
-int a = 0;
-const long N = 100000;
-
+double* result;
+const long N = 100000000;
+double sum = 0.0;
 
 void *mythread(void *dummy)
 
@@ -24,26 +23,41 @@ void *mythread(void *dummy)
    pthread_t mythid;
    int nThread = *(int*)dummy;
    double f0 = 0.0, f1 = 0.0;
-   mythid = pthread_self();
-   for (double j = 2 * mythid / n; j < 2 * (mythid + 1)/n; j += 2.0 / N){
+   int thid = pthread_self();
+   double j = 0.0;
+   for (double j = 2.0 * nThread / numberOfthreads; j < 2.0 * (nThread + 1)/numberOfthreads; j += 2.0 / N){
+
       f1 = sqrt(4 - j*j);
-      result[nThread] += (f1 + f0)/2*(2.0 / N);
+      result[nThread] += (f1 + f0)/2.0*(2.0 / N);
+      f0 = f1;
    }
-   printf("Thread %u, Calculation result = %d\n", mythid, result[nThread]);
+   if (nThread == numberOfthreads) {
+      for (; j < 2; j += 2.0 / N){
+
+         f1 = sqrt(4 - j*j);
+         result[nThread] += (f1 + f0)/2.0*(2.0 / N);
+         f0 = f1;
+   }
+   }
+   printf("Thread %u, Calculation result = %lf\n", nThread, result[nThread]);
 
    return NULL;
 }
 
-int main(int argc, char* argv)
+int main(int argc, char* argv[])
 {
+   // double sum = 0.0;
 	int res = 0;
-	numberOfthreads = argv[1];
+   int numb[10];
+	sscanf(argv[1], "%d", &numberOfthreads);
+   printf ("%d \n", numberOfthreads);
+   // exit(0);
 	pthread_t *thid = (pthread_t*)calloc(numberOfthreads, sizeof(pthread_t)); 
 	result = (double*)calloc(numberOfthreads, sizeof(double));
 
 	for (int j = 0; j < numberOfthreads; j++){
-		int numb = j;
-		res = pthread_create( &thid[i], (pthread_attr_t *)NULL, mythread, &numb);
+		numb[j] = j;
+		res = pthread_create( &(thid[j]), (pthread_attr_t *)NULL, mythread, (void*)&numb[j]);
 
 		if(res != 0){
 			printf ("Error on thread create, return value = %d\n", result);
@@ -54,7 +68,7 @@ int main(int argc, char* argv)
 
 	// printf("Thread created, thid = %u\n", thid);
 
-	printf("Thread %u, Calculation result = %d\n", mythid, a);
+	// printf("Thread %u, Calculation result = %d\n", mythid, a);
 
 
 	for (int j = 0; j < numberOfthreads; j++){
@@ -62,7 +76,9 @@ int main(int argc, char* argv)
 	}
 	for (int j = 0; j < numberOfthreads; j++){
 		sum += result[j];
+      printf ("\n %d %lf \n", j, result[j]);
 	}
 	printf("%lf \n", sum);
+
 	return 0;
 }
